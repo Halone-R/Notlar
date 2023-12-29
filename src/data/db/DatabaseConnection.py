8a09cd666c8d6841f52177af28fc86ee92a36dac
Rajiv Chaves <rajivchaves@Air-de-Rajiv.lan>
2023-12-29 21:55:29 +0000

    Updating code v2

--- src/data/db/DatabaseConnection.py
+++ src/data/db/DatabaseConnection.py
@@ -5,9 +5,42 @@ MONGO_URI =
 # Create a new client and connect to the server
 client = MongoClient(MONGO_URI)
 
+db = client["Notlar"]
+
+collection = db.get_collection("Users")
+notas = db.get_collection("Notas")
+
+notas.insert_one({
+    "Title": "Lista de compras",
+    "Content": "Banana"
+})
+
+'''print(db)
+print()
+print(collection)
+print()
+collection.insert_one({
+    "nome": "Rajiv",
+    "pass": "1234"
+})
+
+collection.delete_one({
+    "nome": "Rajiv"
+})
+
+
+
+response = collection.find({"nome": "Rajiv"})
+
+for elem in response:
+    print(elem)
+
+print()'''
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+
