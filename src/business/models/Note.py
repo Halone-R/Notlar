8d37848b808fefef8bcb549c31dabbfe63411402
Rajiv Chaves <rajivchaves@Air-de-Rajiv.lan>
2024-01-06 04:17:52 +0000

    Remoção de partes desnecessárias e adicionando partes funcionais ao projeto

--- src/business/models/Note.py
+++
@@ -1,7 +0,0 @@
-class Note:
-    def __init__(self, note_id, user_id, title, content, encrypted_content):
-        self.note_id = note_id
-        self.user_id = user_id
-        self.title = title
-        self.content = content
-        self.encrypted_content = encrypted_content
--- src/business/models/User.py
+++
@@ -1,5 +0,0 @@
-class User:
-    def __init__(self, user_id, username, password_hash):
-        self.user_id = user_id
-        self.username = username
-        self.password_hash = password_hash
